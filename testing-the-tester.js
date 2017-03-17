cc9e7eb5d639c3e703e768ca28bebd05ae081a1f
Prima Aulia Gusta <primaulia@gmail.com>
2017-03-17 17:20:34 +0800

    latest code

---
+++ testing-the-tester.js
@@ -0,0 +1,17 @@
+// where you write you javascript codes
+
+function restart() {
+
+}
+
+function isGameOver() {
+  return false
+}
+
+function whoWon() {
+  return 0
+}
+
+function playTurn() {
+  
+}
